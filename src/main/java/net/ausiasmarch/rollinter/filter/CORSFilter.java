082d9329586749fba35a77aef013dc5bb5e98206
esborlan <esborlan.daw@gmail.com>
2023-03-14 15:48:11 +0100

    Añadir enlace vercel

--- src/main/java/net/ausiasmarch/rollinter/filter/CORSFilter.java
+++ src/main/java/net/ausiasmarch/rollinter/filter/CORSFilter.java
@@ -16,16 +16,22 @@ import org.springframework.stereotype.Component;
 public class CORSFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
 
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        String origin = request.getHeader("Origin");
+        if (origin != null && origin.contains("rollinter-client.vercel.app")) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Content-Type, Accept, X-Requested-With, remember-me, Authorization");
         chain.doFilter(req, res);
 
     }
